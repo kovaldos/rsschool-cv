a1b70cd75aacf1bff97705ed9dbeb27de7346c46
Yaroslav Kovalev <yakovaldos@yandex.ru>
2022-06-15 23:41:09 +0300

    feat: add contacts and summary sections

---
+++ rsschool-cv-html/source/js/modules/scroll-to-top.js
@@ -0,0 +1,27 @@
+// Button to top of the viewport - Start -
+import {scrollTo} from '../utils/scroll-to';
+const initBtnToTop = () => {
+  const btnToTop = document.querySelector('.btn__to-top');
+  if (btnToTop) {
+    const scrollHeight = 1500;
+    window.addEventListener('scroll', function () {
+      // Если прокрутили дальше scrollHeight пикселей от верха вьюпорта, показываем кнопку
+      // eslint-disable-next-line no-invalid-this
+      if (this.scrollY > scrollHeight) {
+        btnToTop.classList.add('btn__to-top--is-shown');
+        // Иначе прячем
+      } else {
+        btnToTop.classList.remove('btn__to-top--is-shown');
+      }
+    });
+    // При клике прокручиваем на самый верх
+    btnToTop.addEventListener('click', (e) => {
+      e.preventDefault();
+      // Вызываем функцию, первый аргумент - отступ, второй - скорость скролла, чем больше значение, тем медленнее скорость прокрутки
+      scrollTo(0, 400);
+    });
+  }
+};
+export {initBtnToTop};
+
+// Button to top of the viewport - Finish -
